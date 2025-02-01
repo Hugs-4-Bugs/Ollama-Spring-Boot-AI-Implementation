4e2588d1719e41fb01105b33dd7953a9ed1db7fe
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2025-02-01 18:15:05 +0530

    created by Prabhat

---
+++ src/main/java/com/ollama/backend/Service/AiServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ollama.backend.Service;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+public class AiServiceImpl implements AiService{
+
+    private ChatClient client;
+
+    public AiServiceImpl(ChatClient.Builder builder){
+        this.client = builder.build();
+    }
+
+    @Override
+    public String askAi(String question) {
+        return client.prompt(question).call().content();
+    }
+
+    public Flux<String> streamResponse(String question){
+        return this.client.prompt().user(question).stream().content();
+    }
+}
